cc564c61a760d9d6cb99c64c438b85436878c067
Omar Perez <117095704+perezrangel@users.noreply.github.com>
2024-11-07 23:51:59 +0000

    comments

--- BSTNode.cpp
+++ BSTNode.cpp
@@ -1,8 +1,25 @@
 #include "BSTNode.hpp"
 
+
+/*
+Constructor
+Omar Perez
+Creates node in the subtree rooted at this node 
+Parameters: None
+Return value: None
+*/
 template <typename T>
 BSTNode<T>::BSTNode() : data(T()), left(nullptr), right(nullptr), parent(nullptr) {}
 
+
+
+/*
+Copy Constructor
+Omar Perez
+do this later
+Parameters: None
+Return value: None
+*/
 template <typename T>
 BSTNode<T>::BSTNode(const BSTNode<T>& other) : data(other.data), left(nullptr), right(nullptr), parent(nullptr) {
     if (other.left) {
@@ -48,6 +65,17 @@ BSTNode<T>& BSTNode<T>::operator=(const BSTNode<T>& other) {
     return *this;
 }
 
+
+
+/*
+BSTNode<T>* treeMin()
+treeMin
+Omar Perez
+This function returns a pointer to the node with the minimal value 
+on the tree rooted at 'this'
+Parameters:
+Return Value: pointer to node
+*/
 template <typename T>
 BSTNode<T>* BSTNode<T>::treeMin() {
     BSTNode<T>* current = this;
